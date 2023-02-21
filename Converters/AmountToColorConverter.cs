e43aa018f4effe17bef0418648e658a590ab3407
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-21 17:33:04 -0500

    add converters to the amount

---
+++ Converters/AmountToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace ProsperDaily.Converters
+{
+    public class AmountToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isIncome = ((Label)parameter).Text;
+            var amount = (decimal)value;
+            if (isIncome == "True")
+            {
+                return Colors.DarkGreen;
+            }
+            else
+            {
+                return Colors.DarkRed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/AmountToCurrencyConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace ProsperDaily.Converters
+{
+    public class AmountToCurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isIncome = ((Label)parameter).Text;
+            var amount = (decimal)value;
+            if (isIncome == "True") 
+            {
+                return $"+ {amount:C}";
+            }
+            else
+            {
+                return $"- {amount:C}";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
