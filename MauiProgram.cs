0f9d632dfa1947ad4fb9202ad0009447d7290b33
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-21 14:35:27 -0500

    setting up styles and fonts

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,7 +13,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("Roboto-Black.ttf", "Strong");
+                fonts.AddFont("LibreFranklin-Regular.ttf", "Regular");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
